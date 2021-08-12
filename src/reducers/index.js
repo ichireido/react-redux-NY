f9b7a15eebaa0dc8c8fd9133ce102dd3acb81a7c
ichireido <ichireido@gmail.com>
2021-08-12 17:31:52 +0900

    57 combine reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import number from "./number";
+import title from "./title";
+import day from "./day";
+
+export default combineReducers({
+  number,
+  title,
+  day
+});

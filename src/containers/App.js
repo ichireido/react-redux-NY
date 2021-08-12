344bbac57ad45a3be086179e19b1df945155ccf9
ichireido <ichireido@gmail.com>
2021-08-12 17:36:26 +0900

    57 combine reducer 追加

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,9 @@ import { minus, plus } from "../actions";
 
 const mapStateToProps = (state) => {
   return {
-    number: state
+    number: state.number,
+    day: state.day,
+    title: state.title
   };
 };
 
